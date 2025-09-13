9355bb790dcd41655dcd6bef5dda4457251d836a
Sharadhi Hegde <sharadhih00@gmail.com>
2025-09-13 10:05:44 +0530

    Deployment changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: "https://anonymous-wall-0c87.onrender.com/" 
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
